158a80a6a8d7ed49801064c13b5085f16da0e6fd
thijsBoet <m.boet@chello.nl>
2023-05-15 13:59:12 +0200

    I dont like qwik

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Index = component$(() => {
+    return (
+        <article>
+            <h2>About</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure rerum accusantium ratione natus culpa in.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit quo vero eligendi incidunt rem quae error et eaque consequuntur excepturi necessitatibus itaque officia, officiis at? Velit provident corporis dignissimos eius aperiam assumenda atque, nostrum beatae.</p>
+        </article>
+    )
+});
---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Index = component$(() => {
+    return (
+        <article>
+            <h2>Contact</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit quo vero eligendi incidunt rem quae error et eaque consequuntur excepturi necessitatibus itaque officia, officiis at? Velit provident corporis dignissimos eius aperiam assumenda atque, nostrum beatae.</p>
+        </article>
+    )
+});
